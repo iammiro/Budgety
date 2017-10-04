9c5641e12eee2ba6f8d5a7a2ff36dab3d9ad7f9b
iammiro <mir.kolomiets@gmail.com>
2017-10-04 11:54:09 +0300

    Set first event listeners

--- app.js
+++ app.js
@@ -11,4 +11,21 @@ var UIController = (function () {
 //Global APP Controller
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function () {
+        //    ToDO list
+        //    1. Get the filed input data
+        //    2. Add the item to the budget controller
+        //    3. Add the item to the UI
+        //    4. Calculate the budget
+        //    5. Display the budget on the UI
+    };
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+
 })(budgetController, UIController); 
