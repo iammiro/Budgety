7853a595cd8426fc96fa397bf56ffdefd4a06d34
iammiro <mir.kolomiets@gmail.com>
2017-10-04 12:18:31 +0300

    getting input data from UI

--- app.js
+++ app.js
@@ -6,21 +6,45 @@ var budgetController = (function () {
 //UI Controller
 var UIController = (function () {
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+        },
+
+        getDOMStrings: function () {
+            return DOMStrings;
+        }
+    }
 })();
 
 //Global APP Controller
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function () {
         //    ToDO list
         //    1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
         //    2. Add the item to the budget controller
         //    3. Add the item to the UI
         //    4. Calculate the budget
         //    5. Display the budget on the UI
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
         if (event.keyCode === 13 || event.which === 13) {
